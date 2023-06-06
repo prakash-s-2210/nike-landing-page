02cea66a55d932c222ebbb045e3958d7e9148491
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-06-06 11:01:49 +0530

    footer section

--- app/page.js
+++ app/page.js
@@ -1,28 +1,19 @@
 
 import Nav from "../components/Nav";
-import Collections from '@/components/Collections';
-import Footer from "@/components/Footer";
-import PopularProducts from '@/components/PopularProducts';
-import ProductDetail from '@/components/ProductDetail';
-import Review from "@/components/Review";
-import ShoePromotion from "@/components/ShoePromotion";
-import Subscribe from "@/components/Subscribe";
-import SupportSection from "@/components/SupportSection";
-import { Hero } from "@/sections";
+import { Footer, Hero } from "@/sections";
 
 export default function Home() {
   return (
     <div>
       <Nav />
       <Hero />
-      {/* <Collections /> */}
+      <Footer />
       {/* <PopularProducts /> */}
       {/* <ProductDetail /> */}
       {/* <SupportSection /> */}
       {/* <ShoePromotion /> */}
       {/* <Review /> */}
       {/* <Subscribe /> */}
-      {/* <Footer /> */}
     </div>
   )
 }
--- sections/index.js
+++ sections/index.js
@@ -1,5 +1,7 @@
 import Hero from "./Hero";
+import Footer from "./Footer";
 
 export {
-    Hero
+    Hero,
+    Footer
 }
